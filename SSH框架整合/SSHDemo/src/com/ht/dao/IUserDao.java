cc34bf80cfd95f0142c45323f665609b7ff8e9a4
LiuXu <1991891370@qq.com>
2018-06-11 11:12:36 +0800

    SSH框架整合实例

--- SSH框架整合/SSHDemo/src/com/ht/dao/IUserDao.java
+++
@@ -1,8 +0,0 @@
-package com.ht.dao;
-
-import com.ht.base.IBaseDao;
-import com.ht.model.User;
-
-public interface IUserDao extends IBaseDao<Integer, User> {
-
-}
--- SSH框架整合/SSHDemo/src/com/ht/service/IUserService.java
+++
@@ -1,8 +0,0 @@
-package com.ht.service;
-
-import com.ht.base.IBaseService;
-import com.ht.model.User;
-
-public interface IUserService extends IBaseService<Integer, User> {
-	
-}
